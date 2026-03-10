c3a815d09ac0a99ba7df7b86d2cc84082b3c18a8
Jello-idir <abdeljalil.idir@gmail.com>
2026-03-10 03:29:33 +0000

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -6,11 +6,12 @@ int main(int argc, char *argv[]) {
     char command[1024];
 
     setbuf(stdout, NULL);
-    printf("$ ");
-
-    fgets(command, sizeof(command), stdin);
-    command[strcspn(command, "\n")] = 0;
-    printf("%s: command not found\n", command);
-
+    while(1)
+    {
+        printf("$ ");
+        fgets(command, sizeof(command), stdin);
+        command[strcspn(command, "\n")] = 0;
+        printf("%s: command not found\n", command);
+    }
     return 0;
 }
