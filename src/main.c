9c776763c26785dcb584254753e6d8c12870f5d5
Jello-idir <abdeljalil.idir@gmail.com>
2026-03-10 04:02:55 +0000

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -2,18 +2,40 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main(int argc, char *argv[]) {
-    char command[1024];
+void echo(char *input)
+{
+    printf("%s\n", input + 5);
+}
 
-    setbuf(stdout, NULL);
+int bridge(char *cmdline)
+{
+    if (!strcmp(cmdline, "exit"))
+        return 1;
+    if (!strncmp(cmdline, "echo ", 5))
+        echo(cmdline);
+    else
+        printf("%s: command not found\n", cmdline);
+    return 0;
+}
+
+void repl()
+{
+    char cmdline[1024];
+
     while(1)
     {
         printf("$ ");
-        fgets(command, sizeof(command), stdin);
-        command[strcspn(command, "\n")] = 0;
-        if (!strcmp(command, "exit"))
-            break;
-        printf("%s: command not found\n", command);
+        if (!fgets(cmdline, sizeof(cmdline), stdin))
+            return;
+        cmdline[strcspn(cmdline, "\n")] = 0;
+        if (bridge(cmdline))
+            return ;
     }
+}
+
+int main(int argc, char *argv[]) {
+
+    setbuf(stdout, NULL);
+    repl();
     return 0;
 }
